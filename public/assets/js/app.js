0f2bd71f36dd152f0e242f1712423bca999616eb
PiotrBerebecki <petebere@gmail.com>
2017-02-12 22:42:57 +0000

    Show video in canvas

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,40 @@
 console.clear();
 
-console.log('Hello');
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      // console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+
+function paintToCanvas() {
+  const { videoWidth: width, videoHeight: height } = video;
+  console.log(width, height);
+  [canvas.width, canvas.height] = [width, height];
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 40);
+}
+
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+}
+
+
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
